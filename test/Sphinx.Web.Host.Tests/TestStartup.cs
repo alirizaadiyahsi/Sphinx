6447f4bce2703a802880dcd24cce1d954db6833c
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-04-01 20:26:58 +0300

    #3 - Added integration test for host project

---
+++ test/Sphinx.Web.Host.Tests/TestStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sphinx.Web.Host.Tests
+{
+    public class TestStartup : Startup
+    {
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+        }
+
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
